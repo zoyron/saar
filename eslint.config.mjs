9e1196247c0ff4744ad4fb1e7d3143441d875ac4
zoyron <sgrarora3@gmail.com>
2025-05-20 03:12:53 +0530

    install shadcn, and add a button

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = {
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    plugins: ["prettier"],
+    rules: {
+      "prettier/prettier": "error",
+      "react/no-escape=entities": off,
+    },
+  }),
+};
 
 export default eslintConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Home
+      <Button variant={"outline"} size={"lg"}>
+        Shadcn Button
+      </Button>
     </div>
   );
 }
