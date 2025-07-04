d89ae54596f4789765b5e2f302bdb923f79c79f8
zoyron <sgrarora3@gmail.com>
2025-07-04 16:23:22 +0530

    initiate header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Source_Sans_3 as FontSans } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/common/header";
 
 const fontSans = FontSans({
   variable: "--font-sans",
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${fontSans.variable} font-sans antialiased`}>
+        <Header />
         {children}
       </body>
     </html>
